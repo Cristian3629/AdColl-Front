ca0ee18a5c4f364ff98d6efecbf5a313631cb270
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-26 14:30:41 -0300

    falta arreglar como vincular bien el vue object

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-var app = new Vue({
-  el: '#app',
+var header_us = new Vue({
+  el:'#us',
   data:{
-    message:'Hello Vue JS'
+    isActive:true,
+  },
+  methods:{
+    activeElement:function(){
+      isActive = true;
+    }
+  }
+})
+
+
+var header_register = new Vue({
+  el:'#register',
+  data:{
+    active:false
+  },
+  methods:{
+    activeElement:function()
+  }
+})
+
+var header_login = new Vue({
+  el :'#login',
+  data:{
+    active:false
+  }
+})
+
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    seen: true
+  }
+})
+
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    seen: true
   }
 })
